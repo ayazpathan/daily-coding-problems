cb05ab9be385a392b68b35c54cf6f0a3d80475c5
Aiyazkhan Pathan <pathan.ayaz@outlook.com>
2021-08-08 19:53:28 +0530

    Working on first coding problem

---
+++ Arrays/one.py
@@ -0,0 +1,17 @@
+test_prefix = []
+def products(nums): 
+    # Generate prefix products. 
+    prefix_products = [] 
+    
+    for num in nums: 
+        if prefix_products: 
+            prefix_products.append(prefix_products[-1] * num) 
+        else: 
+            prefix_products.append(num)
+    print(prefix_products);
+
+    print(prefix_products);
+
+    
+products([1, 2, 3, 4, 5])
+    
