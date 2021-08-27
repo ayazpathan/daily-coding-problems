b9ad44174d20836eb394674e5de532a189385972
ayazpathan <pathan.ayaz@outlook.com>
2021-08-27 23:13:46 +0530

    Solved cosumer problem

--- Arrays/one.py
+++ Arrays/one.py
@@ -10,10 +10,31 @@ def products(nums):
             prefix_products.append(prefix_products[-1] * num) 
         else: 
             prefix_products.append(num)
-    print(prefix_products);
+        
+    # Generate suffix products. 
+    suffix_products = [] 
 
-    print(prefix_products);
+    for num in reversed(nums):
+        if suffix_products:
+            suffix_products.append(suffix_products[-1] * num)
+        else:
+            suffix_products.append(num)
+
+    suffix_products = list(reversed(suffix_products))
+
+    # Generate result from the product of prefixes and suffixes. 
+    result= [] 
+    for i in range(len(nums)):
+        if i == 0:
+            result.append(suffix_products[i + 1])
+        elif i == len(nums) - 1:
+            result.append(prefix_products[i - 1])
+        else:
+            result.append(prefix_products[i - 1] * suffix_products[i + 1])
+   
+    return result
+
 
     
-products([1, 2, 3, 4, 5])
+print(products([1, 2, 3, 4, 5]))
     
