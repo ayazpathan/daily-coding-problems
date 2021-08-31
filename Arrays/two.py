0094e67143956052e5676f61db54026251d52bb6
ayazpathan <pathan.ayaz@outlook.com>
2021-08-31 22:13:15 +0530

    Fixed smallest window to be stored problem

--- Arrays/two.py
+++ Arrays/two.py
@@ -6,3 +6,22 @@ given [ 3 , 7 , 5 , 6 , 9] , you should return ( 1 , 3 )
 
 """
 
+def window(array):
+    left, right = None, None
+    n = len(array)
+    max_seen, min_seen = -float("inf"), float("inf")
+
+    for i in range(n):
+        max_seen = max(max_seen, array[i])
+        if array[i] < max_seen:
+            right = i
+        
+    for i in range(n - 1, -1, -1):
+        min_seen = min(min_seen, array[i])
+        if array[i] > min_seen:
+            left = i
+
+    return left, right
+
+
+print(window([3, 7, 5, 6, 9]))
